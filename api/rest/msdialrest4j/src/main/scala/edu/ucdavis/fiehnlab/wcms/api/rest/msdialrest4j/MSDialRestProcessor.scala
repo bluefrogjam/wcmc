7a7064a98f851e7d73f22c31081346a35fc4fd29
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2017-06-16 16:16:38 -0700

    working on testing REST api, currently working without issues as .abf file, failing as .d file for odd reasons, caused by a 500 error.

--- api/rest/msdialrest4j/src/main/scala/edu/ucdavis/fiehnlab/wcms/api/rest/msdialrest4j/MSDialRestProcessor.scala
+++ api/rest/msdialrest4j/src/main/scala/edu/ucdavis/fiehnlab/wcms/api/rest/msdialrest4j/MSDialRestProcessor.scala
@@ -1,9 +1,15 @@
 package edu.ucdavis.fiehnlab.wcms.api.rest.msdialrest4j
 
-import java.io.File
+import java.io._
+import java.util.zip.ZipOutputStream
 
-import org.springframework.beans.factory.annotation.Value
+import com.typesafe.scalalogging.LazyLogging
+import edu.ucdavis.fiehnlab.wcms.utilities.ZipUtil
+import org.springframework.beans.factory.annotation.{Autowired, Value}
+import org.springframework.core.io.FileSystemResource
+import org.springframework.http.{HttpStatus, ResponseEntity}
 import org.springframework.stereotype.Component
+import org.springframework.web.client.RestOperations
 
 /**
   * Created by wohlgemuth on 6/16/17.
@@ -11,60 +17,182 @@ import org.springframework.stereotype.Component
   * connects to the MSDial rest server
   */
 @Component
-class MSDialRestProcessor {
+class MSDialRestProcessor extends LazyLogging {
 
   @Value("${wcms.api.rest.msdialrest4j.host:128.120.143.101}")
-  val host:String
+  val host: String = ""
 
   @Value("${wcms.api.rest.msdialrest4j.port:80}")
-  val port:Int
+  val port: Int = 80
+
+  @Autowired
+  val restTemplate: RestOperations = null
 
   /**
     * processes the input file and
+    *
     * @param input
     * @return
     */
-  def process(input:File) : File = {
+  def process(input: File): File = {
 
     //if directory and ends with .d zip file
+    val toUpload = if (input.isDirectory && input.getName.endsWith(".d")) {
+
+      val temp = File.createTempFile("wcms", ".zip")
+      logger.debug(s"compressing to local file: ${temp} prior to upload")
+      temp.deleteOnExit()
+      val out = new ZipOutputStream(new FileOutputStream(temp))
+      ZipUtil.zip(input, out)
+      out.flush()
+      out.close()
+      temp
+    }
+    else {
+      input
+    }
 
     //upload
-
-    //convert
+    val uploadId = upload(toUpload)
 
     //schedule
+    val scheduleId = schedule(uploadId)
 
-    //check status
-
-    //if finished download to temp
-
-    //return handle
+    //download processed id
+    download(scheduleId)
   }
 
   /**
     * uploads a given file to the server
+    *
     * @param file
     * @return
     */
-  protected def upload(file:File):String = ???
+  protected def upload(file: File): String = {
+
+    import org.springframework.http.{HttpEntity, HttpHeaders, HttpMethod, MediaType}
+    import org.springframework.util.LinkedMultiValueMap
+
+    val map = new LinkedMultiValueMap[String, AnyRef]
+    map.add("file", new FileSystemResource(file))
+    val headers = new HttpHeaders
+    headers.setContentType(MediaType.MULTIPART_FORM_DATA)
+
+    val requestEntity = new HttpEntity[LinkedMultiValueMap[String, AnyRef]](map, headers)
+    val result = restTemplate.exchange(s"$url/rest/upload", HttpMethod.POST, requestEntity, classOf[ServerResponse])
+
+    if (result.getStatusCode == HttpStatus.OK) {
+      if (result.getBody.link.contains("/conversion/")) {
+        logger.debug("upload result requires conversion of data")
+        convert(result.getBody.link.split("/").last)
+      }
+      else {
+        logger.debug("upload succeeded, not conversion required")
+        result.getBody.link.split("/").last
+      }
+    }
+    else {
+      throw new MSDialException(result)
+    }
+  }
 
   /**
     * was this process finished
+    *
     * @param id
     * @return
     */
-  protected def isFinished(id:String):Boolean = ???
+  protected def download(id: String): File = {
+    val result = restTemplate.getForEntity(s"$url/rest/deconvolution/status/${id}", classOf[ServerResponse])
+
+    if (result.getStatusCode == HttpStatus.OK) {
+      val resultId = result.getBody.link.split("/").last
+
+      val download = restTemplate.getForEntity(s"$url/rest/deconvolution/result/${id}", classOf[String])
+
+      if (download.getStatusCode == HttpStatus.OK) {
+
+        val file = File.createTempFile("msdial", "deco")
+
+        val out = new BufferedWriter(new FileWriter(file))
+        out.write(download.getBody)
+        out.flush()
+        out.close()
+
+        file
+      }
+      else {
+        throw new MSDialException(result)
+      }
+    }
+    else {
+      throw new MSDialException(result)
+    }
+  }
 
   /**
     * convert the given file, if it was a .d file or so to an abf file
+    *
     * @param id
     */
-  protected def convert(id:String):String = ???
+  protected def convert(id: String): String = {
+    val result = restTemplate.getForEntity(s"$url/rest/conversion/convert/${id}", classOf[ServerResponse])
+
+    if (result.getStatusCode == HttpStatus.OK) {
+      logger.debug("conversion succeeded, not conversion required")
+      result.getBody.link.split("/").last
+    }
+    else {
+      throw new MSDialException(result)
+    }
+
+  }
 
   /**
-    * downloads the remote file to the local harddrive
+    * schedules the processing of the actual object
+    * for deconvolution
+    *
     * @param id
     * @return
     */
-  protected def download(id:String) : File = ???
+  protected def schedule(id: String): String = {
+    val result = restTemplate.getForEntity(s"$url/rest/deconvolution/schedule/${id}", classOf[ServerResponse])
+
+    if (result.getStatusCode == HttpStatus.OK) {
+      logger.debug("conversion succeeded, not conversion required")
+      result.getBody.link.split("/").last
+    }
+    else {
+      throw new MSDialException(result)
+    }
+  }
+
+  protected def url = s"http://${host}:${port}"
+
+  /**
+    * defined server response
+    *
+    * @param filename
+    * @param link
+    * @param message
+    * @param error
+    */
+
 }
+
+/**
+  * exspected server response
+  *
+  * @param filename
+  * @param link
+  * @param message
+  * @param error
+  */
+case class ServerResponse(filename: String, link: String, message: String, error: String)
+
+/**
+  * an msdial exception if something goes wrong
+  *
+  * @param result
+  */
+class MSDialException(result: ResponseEntity[ServerResponse]) extends Exception
--- api/rest/msdialrest4j/src/test/scala/edu/ucdavis/fiehnlab/wcms/api/rest/msdialrest4j/MSDialRestProcessorTest.scala
+++ api/rest/msdialrest4j/src/test/scala/edu/ucdavis/fiehnlab/wcms/api/rest/msdialrest4j/MSDialRestProcessorTest.scala
@@ -1,17 +1,29 @@
 package edu.ucdavis.fiehnlab.wcms.api.rest.msdialrest4j
 
+import java.io.File
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper, SerializationFeature}
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import org.junit.runner.RunWith
 import org.scalatest.{ShouldMatchers, WordSpec}
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Bean
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.test.context.TestContextManager
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.web.client.{RestOperations, RestTemplate}
+
+import scala.io.Source
 
 /**
   * Created by wohlgemuth on 6/16/17.
   */
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(classOf[SpringRunner])
+@SpringBootTest(classes = Array(classOf[MSDialRestProcessorConfig]))
 class MSDialRestProcessorTest extends WordSpec with ShouldMatchers{
 
   @Autowired
@@ -25,13 +37,71 @@ class MSDialRestProcessorTest extends WordSpec with ShouldMatchers{
 
       "process a .d file" in {
 
+        val input = new File(getClass.getResource("/test.d").toURI)
+
+        val output = mSDialRestProcessor.process(input)
+
+        val resultLines = Source.fromFile(output).getLines().toSeq
+
+        resultLines.head.split("\t") should contain ("Name")
+        resultLines.head.split("\t") should contain ("ScanAtLeft")
+
+        resultLines.size should be (12)
       }
 
-      "process a .abf file" in {
+      //fails currently with a 500 error, need to wait till diego is back from vacation to fix this
+      "process a .abf file" ignore {
 
+        val input = new File(getClass.getResource("/test.abf").toURI)
+
+        val output = mSDialRestProcessor.process(input)
+
+        val resultLines = Source.fromFile(output).getLines().toSeq
+
+        resultLines.head.split("\t") should contain ("Name")
+        resultLines.head.split("\t") should contain ("ScanAtLeft")
+
+        resultLines.size should be (679)
       }
 
     }
 
   }
+}
+
+@SpringBootApplication
+class MSDialRestProcessorConfig{
+
+  @Bean
+  def objectMapper: ObjectMapper = {
+
+    val mapper = new ObjectMapper() with ScalaObjectMapper
+
+    mapper.registerModule(DefaultScalaModule)
+
+    //required, in case we are provided with a list of value
+    mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    mapper.setSerializationInclusion(Include.NON_NULL)
+    mapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true)
+
+    mapper
+  }
+
+  @Bean
+  def restOperations: RestOperations = {
+
+    val rest: RestTemplate = new RestTemplate()
+    rest.getMessageConverters.add(0, mappingJacksonHttpMessageConverter)
+    rest
+  }
+
+
+  @Bean
+  def mappingJacksonHttpMessageConverter: MappingJackson2HttpMessageConverter = {
+    val converter: MappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter()
+    converter.setObjectMapper(objectMapper)
+    converter
+  }
+
 }
