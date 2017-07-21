cfd79292144b02b17d89f3f631128930ea811496
Diego Pedrosa <linuxmant@gmail.com>
2017-07-21 14:49:48 -0700

    updated ABFSample to the new MSDIAL deconvolution format, adding support to read MSMS spectrum

--- pipeline/core/io/abf/src/main/scala/edu/ucdavis/fiehnlab/ms/carrot/core/api/io/abf/ABFSample.scala
+++ pipeline/core/io/abf/src/main/scala/edu/ucdavis/fiehnlab/ms/carrot/core/api/io/abf/ABFSample.scala
@@ -20,8 +20,10 @@ class ABFSample(override val fileName:String,file:File, client:MSDialRestProcess
 
   def deconvolute:Seq[_ <:Feature] = {
     logger.debug("converting to MSDial representation")
-    MSDialSample(fileName,client.process(file)).spectra
-
+	  val start = System.nanoTime()
+	  val spec = MSDialSample(fileName, client.process(file)).spectra
+	  logger.info(s"preprocess took: ${(System.nanoTime() - start) / 1000000000}s")
+	  spec
   }
   /**
     * all the deconvolution spectra for this file. Defined as lazy to reduce memory usage
