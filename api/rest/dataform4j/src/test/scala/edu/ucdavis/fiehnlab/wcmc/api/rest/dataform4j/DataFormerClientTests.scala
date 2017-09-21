a687ce7a16b18263b71053ba69f421dc276e15f0
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2017-09-21 16:27:13 -0700

    Fixed config for embedded server

--- api/rest/dataform4j/src/test/scala/edu/ucdavis/fiehnlab/wcmc/api/rest/dataform4j/DataFormerClientTests.scala
+++ api/rest/dataform4j/src/test/scala/edu/ucdavis/fiehnlab/wcmc/api/rest/dataform4j/DataFormerClientTests.scala
@@ -1,9 +1,6 @@
 package edu.ucdavis.fiehnlab.wcmc.api.rest.dataform4j
 
-import java.io.File
-
 import com.typesafe.scalalogging.LazyLogging
-import edu.ucdavis.fiehnlab.loader.impl.RecursiveDirectoryResourceLoader
 import edu.ucdavis.fiehnlab.wcmc.api.rest.fserv4j.FServ4jClient
 import edu.ucdavis.fiehnlab.wcmc.server.fserv.{FServ, FServSecurity}
 import org.junit.runner.RunWith
@@ -11,35 +8,33 @@ import org.scalatest.{ShouldMatchers, WordSpec}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.context.embedded.LocalServerPort
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 import org.springframework.context.annotation.{Bean, ComponentScan, Configuration}
 import org.springframework.test.context.TestContextManager
 import org.springframework.test.context.junit4.SpringRunner
-import org.springframework.web.client.RestTemplate
 
 /**
 	* Created by diego on 8/31/2017.
 	*/
 @RunWith(classOf[SpringRunner])
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = Array(classOf[FServ], classOf[FServSecurity], classOf[DataFormerClientConfiguration]))
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT, classes = Array(classOf[FServ], classOf[FServSecurity], classOf[DataFormerClientConfiguration]))
 class DataFormerClientTests extends WordSpec with ShouldMatchers with LazyLogging {
 	@Autowired
 	val dfClient: DataFormerClient = null
 
-	@Autowired
-	val resourceLoader: RecursiveDirectoryResourceLoader = null
+	@LocalServerPort
+	val port: Int = 0
 
 	new TestContextManager(this.getClass).prepareTestInstance(this)
 
-	"edu.ucdavis.fiehnlab.wcmc.api.rest.dataform4j.DataFormerClient" ignore {
+	"edu.ucdavis.fiehnlab.wcmc.api.rest.dataform4j.DataFormerClient" should {
 		"upload a raw data file" in {
-			val rawfile = resourceLoader.loadAsFile("B5_P20Lipids_Pos_QC029.d.zip")
-			rawfile should not be null
 
-			val result = dfClient.upload(rawfile.get)
+			val result = dfClient.convert("B5_P20Lipids_Pos_QC029.d.zip")
 			logger.debug(s"result $result")
-			result should contain("uploaded successfully")
+			result should contain("{\"abfFile\":\"B5_P20Lipids_Pos_QC029.abf\", \"mzml\":\"B5_P20Lipids_Pos_QC029.mzml\"")
 
 		}
 	}
@@ -52,12 +47,10 @@ class DataFormerClientConfiguration {
 	@Bean
 	def dfClient: DataFormerClient = new DataFormerClient()
 
-	@Bean
-	def restTemplate: RestTemplate = new RestTemplate()
+	//	@Bean
+	//	def restTemplate: RestTemplate = new RestTemplate()
 
 	@Bean
 	def fserv4j: FServ4jClient = new FServ4jClient()
 
-	@Bean
-	def resourceLoader: RecursiveDirectoryResourceLoader = new RecursiveDirectoryResourceLoader(new File("/h/p20repo"))
 }
