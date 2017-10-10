387c379faee0881aa34535235492757839e90a70
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2017-10-10 13:08:47 -0700

    added support for everything4j to the system

---
+++ api/rest/everything4j/src/main/java/edu/ucdavis/fiehnlab/wcmc/api/rest/everything4j/Everything4J.scala
@@ -0,0 +1,82 @@
+package edu.ucdavis.fiehnlab.wcmc.api.rest.everything4j
+
+import java.io.InputStream
+import java.net.{URI, URL, URLEncoder}
+import java.nio.charset.StandardCharsets
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.typesafe.scalalogging.LazyLogging
+import edu.ucdavis.fiehnlab.loader.RemoteLoader
+import edu.ucdavis.fiehnlab.wcmc.utilities.casetojson.config.CaseClassToJSONSerializationConfiguration
+import org.springframework.beans.factory.annotation.{Autowired, Value}
+import org.springframework.context.annotation.{Configuration, Import}
+import org.springframework.stereotype.Component
+import org.springframework.web.util.UriUtils
+
+/**
+  * Created by wohlgemuth on 10/10/17.
+  */
+@Component
+class Everything4J extends RemoteLoader with LazyLogging{
+
+  @Value("${wcmc.api.rest.everything4j.host:luna.fiehnlab.ucdavis.edu}")
+  val host: String = ""
+
+  @Value("${wcmc.api.rest.everything4j.port:80}")
+  val port: Int = 80
+
+  @Autowired
+  val objectMapper:ObjectMapper = null
+
+  /**
+    * returns the related resource or none
+    *
+    * @param name
+    * @return
+    */
+  override def load(name: String): Option[InputStream] = {
+    val url = s"http://${host}:${port}?s=${name}&j=1&path_column=1"
+    logger.info(s"checking url: ${url}")
+
+    val data = objectMapper.readValue(new URL(url),classOf[Search]).results.filter(_.`type`.toLowerCase() == "file")
+
+    if(data.isEmpty){
+      None
+    }
+    else{
+      val encoded = data.head.path.replaceAll("\\\\","/").replaceAll("\\s","%20").replaceAll(":","%3A")
+      val content = new URI(s"http://${host}:${port}/${encoded}").toURL
+
+      Option(content.openStream())
+
+    }
+  }
+
+  /**
+    * does the given resource exists
+    *
+    * @param name
+    * @return
+    */
+  override def exists(name: String): Boolean = {
+    val url = s"http://${host}:${port}?s=${name}&j=1&path_column=1"
+    logger.info(s"checking url: ${url}")
+
+    objectMapper.readValue(new URL(url), classOf[Search]).results.exists(_.`type`.toLowerCase() == "file")
+
+  }
+}
+
+@Configuration
+@Import(Array(classOf[CaseClassToJSONSerializationConfiguration]))
+class Everything4JConfiguration
+
+case class Results(
+                    `type`: String,
+                    name: String,
+                    path:String
+                  )
+case class Search(
+                   totalResults: Double,
+                   results: List[Results]
+                 )
---
+++ api/rest/everything4j/src/test/java/edu/ucdavis/fiehnlab/wcmc/api/rest/everything4j/Everything4JTest.scala
@@ -0,0 +1,50 @@
+package edu.ucdavis.fiehnlab.wcmc.api.rest.everything4j
+
+import com.typesafe.scalalogging.LazyLogging
+import org.junit.runner.RunWith
+import org.scalatest.{BeforeAndAfterEach, ShouldMatchers, WordSpec}
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.{ComponentScan, Configuration}
+import org.springframework.test.context.TestContextManager
+import org.springframework.test.context.junit4.SpringRunner
+
+/**
+  * Created by wohlgemuth on 10/10/17.
+  */
+@RunWith(classOf[SpringRunner])
+@SpringBootTest(classes = Array(classOf[TestConfig]))
+class Everything4JTest extends WordSpec  with ShouldMatchers with BeforeAndAfterEach with LazyLogging{
+
+  @Autowired
+  val everything4J:Everything4J = null
+
+  new TestContextManager(this.getClass).prepareTestInstance(this)
+
+  "Everything4JTest" should {
+
+    "load" in {
+      everything4J.load("090309bsesa100_1.cdf").isDefined shouldBe true
+    }
+
+    "exists" must {
+
+      "have file " in {
+        everything4J.exists("090309bsesa100_1.cdf") shouldBe true
+      }
+
+      "have not file " in {
+        everything4J.exists("090309bsesa100_1.cdf.D") shouldBe false
+      }
+
+
+    }
+
+  }
+}
+
+@Configuration
+@ComponentScan
+class TestConfig{
+
+}
