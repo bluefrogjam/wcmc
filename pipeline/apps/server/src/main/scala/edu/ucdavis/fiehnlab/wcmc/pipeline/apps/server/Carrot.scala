e5057951d45675c7f456fc8e24b4e488e164ff41
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2017-10-10 12:10:41 -0700

    trying to simplify the rest server

--- pipeline/apps/server/src/main/scala/edu/ucdavis/fiehnlab/wcmc/pipeline/apps/server/Carrot.scala
+++ pipeline/apps/server/src/main/scala/edu/ucdavis/fiehnlab/wcmc/pipeline/apps/server/Carrot.scala
@@ -1,18 +1,16 @@
 package edu.ucdavis.fiehnlab.wcmc.pipeline.apps.server
 
 import com.typesafe.scalalogging.LazyLogging
-import edu.ucdavis.fiehnlab.loader.DelegatingResourceLoader
 import edu.ucdavis.fiehnlab.ms.carrot.core.db.mona.MonaLibraryAccessConfiguration
 import edu.ucdavis.fiehnlab.ms.carrot.core.schedule.ScheduleConfig
-import edu.ucdavis.fiehnlab.ms.carrot.core.workflow.targeted.LCMSPositiveModeTargetWorkflow
 import edu.ucdavis.fiehnlab.ms.carrot.core.workflow.CentralWorkflowConfig
-import edu.ucdavis.fiehnlab.wcmc.pipeline.apps.server.controller.SchedulingController
+import edu.ucdavis.fiehnlab.ms.carrot.core.workflow.targeted.LCMSPositiveModeTargetWorkflow
 import edu.ucdavis.fiehnlab.wcmc.server.fserv.controller.FServController
-import org.springframework.beans.factory.annotation.Autowired
+import edu.ucdavis.fiehnlab.wcmc.utilities.casetojson.config.CaseClassToJSONSerializationConfiguration
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
-import org.springframework.boot.autoconfigure.{EnableAutoConfiguration, SpringBootApplication}
-import org.springframework.context.annotation.{Bean, Configuration}
+import org.springframework.context.annotation.{Bean, ComponentScan, Configuration}
 import org.springframework.http.MediaType
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.{EnableWebSecurity, WebSecurityConfigurerAdapter}
@@ -21,13 +19,22 @@ import org.springframework.web.servlet.config.annotation.{ContentNegotiationConf
 /**
   * Created by wohlgemuth on 9/7/17.
   */
-@SpringBootApplication(scanBasePackageClasses = Array(classOf[SchedulingController], classOf[ScheduleConfig], classOf[FServController], classOf[CentralWorkflowConfig], classOf[MonaLibraryAccessConfiguration],classOf[Carrot]))
-@EnableAutoConfiguration(exclude = Array(classOf[DataSourceAutoConfiguration]))
+@SpringBootApplication(scanBasePackageClasses = Array(
+  classOf[ScheduleConfig],
+  classOf[FServController],
+  classOf[CentralWorkflowConfig],
+  classOf[MonaLibraryAccessConfiguration],
+  classOf[CarrotCors],
+  classOf[CarrotSecurity],
+  classOf[CaseClassToJSONSerializationConfiguration]), exclude =Array(classOf[DataSourceAutoConfiguration])
+)
 class Carrot {
 
-  @Autowired
-  val resourceLoader: DelegatingResourceLoader = null
-
+  /**
+    * should be done over a profile TODO
+    *
+    * @return
+    */
   @Bean
   def workflow: LCMSPositiveModeTargetWorkflow[Double] = {
     new LCMSPositiveModeTargetWorkflow()
@@ -42,21 +49,22 @@ object Carrot extends App {
 }
 
 @Configuration
+@ComponentScan
 class CarrotCors extends WebMvcConfigurerAdapter {
 
-	override def configureContentNegotiation(configurer: ContentNegotiationConfigurer): Unit = {
-		configurer.favorPathExtension(false).favorParameter(false).parameterName("mediaType").ignoreAcceptHeader(false).useJaf(false).defaultContentType(MediaType.APPLICATION_JSON).mediaType("json", MediaType.APPLICATION_JSON)
-	}
+  override def configureContentNegotiation(configurer: ContentNegotiationConfigurer): Unit = {
+    configurer.favorPathExtension(false).favorParameter(false).parameterName("mediaType").ignoreAcceptHeader(false).useJaf(false).defaultContentType(MediaType.APPLICATION_JSON).mediaType("json", MediaType.APPLICATION_JSON)
+  }
 
-	override def addCorsMappings(registry: CorsRegistry): Unit = registry.addMapping("/**")
+  override def addCorsMappings(registry: CorsRegistry): Unit = registry.addMapping("/**")
 }
 
 @Configuration
 @EnableWebSecurity
 class CarrotSecurity extends WebSecurityConfigurerAdapter with LazyLogging {
 
-	override def configure(web: WebSecurity): Unit = {
-		logger.warn("we are allowing unregulated access to this service!")
-		web.ignoring().antMatchers("/**")
-	}
+  override def configure(web: WebSecurity): Unit = {
+    logger.warn("we are allowing unregulated access to this service!")
+    web.ignoring().antMatchers("/**")
+  }
 }
