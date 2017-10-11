45313312a460c61f8ff7b49ede6262f13c2a56ac
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2017-10-11 07:46:59 -0700

    made everything4j spring autoconfiguration compliant

---
+++ api/rest/everything4j/src/main/java/edu/ucdavis/fiehnlab/wcmc/api/rest/everything4j/Everything4JAutoConfiguration.scala
@@ -0,0 +1,13 @@
+package edu.ucdavis.fiehnlab.wcmc.api.rest.everything4j
+
+import org.springframework.context.annotation.{Bean, ComponentScan, Configuration}
+
+/**
+  * Created by wohlgemuth on 10/11/17.
+  */
+@Configuration
+class Everything4JAutoConfiguration {
+
+  @Bean
+  def eveyrthing4J: Everything4J = new Everything4J()
+}
