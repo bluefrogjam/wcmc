7949bda2b97ad6e936c08824d0e28a15e33e0565
Diego Pedrosa <linuxmant@gmail.com>
2017-10-20 11:29:46 -0700

    Removed dataform server module Removed unused SpectrumMinimizer related classes

--- api/rest/msdialrest4j/src/test/scala/edu/ucdavis/fiehnlab/wcmc/api/rest/msdialrest4j/MSDialRestProcessorTest.scala
+++ api/rest/msdialrest4j/src/test/scala/edu/ucdavis/fiehnlab/wcmc/api/rest/msdialrest4j/MSDialRestProcessorTest.scala
@@ -1,16 +1,16 @@
 package edu.ucdavis.fiehnlab.wcmc.api.rest.msdialrest4j
 
-import java.io.File
+import java.io.{File, FileOutputStream}
 
 import com.typesafe.scalalogging.LazyLogging
-import edu.ucdavis.fiehnlab.wcmc.utilities.casetojson.config.CaseClassToJSONSerializationAutoConfiguration
+import edu.ucdavis.fiehnlab.wcmc.api.rest.fserv4j.FServ4jClient
+import org.apache.commons.io.IOUtils
 import org.junit.runner.RunWith
 import org.scalatest.{ShouldMatchers, WordSpec}
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.{EnableAutoConfiguration, SpringBootApplication}
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.{EnableAutoConfiguration, SpringBootApplication}
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Import
 import org.springframework.test.context.TestContextManager
 import org.springframework.test.context.junit4.SpringRunner
 
@@ -26,20 +26,49 @@ class MSDialRestProcessorTest extends WordSpec with LazyLogging with ShouldMatch
   @Autowired
   val mSDialRestProcessor:MSDialRestProcessor = null
 
+  @Autowired
+  val fserv4jCli: FServ4jClient = null
+
+  def sha256Hash(text: String): String = String.format("%064x", new java.math.BigInteger(1, java.security.MessageDigest.getInstance("SHA-256").digest(text.getBytes("UTF-8"))))
+
   new TestContextManager(this.getClass).prepareTestInstance(this)
 
   "MSDialRestProcessorTest" ignore {
 
-    "process" must {
+    "upload a file to the server" in {
+      val filename = "testA.d"
+      if (!fserv4jCli.exists(filename)) {
+        fail(s"can't load ${filename}")
+      }
 
+      val file: File = new File(s"${filename}.zip")
+      val source = fserv4jCli.download(filename).getOrElse(fail(s"can't download ${filename}"))
+      val target = new FileOutputStream(file)
+      try {
+        IOUtils.copy(source, target)
+      } finally {
+        target.flush()
+        target.close()
+      }
+
+      val (uploadRes, token) = mSDialRestProcessor.upload(file)
+
+      uploadRes shouldEqual sha256Hash("testA.d").substring(0, 9)
+      token shouldEqual "testA.d"
+    }
+
+    "process" must {
       "process a .d file" in {
 
-        val input = new File(getClass.getResource("/test.d").toURI)
+        val input = new File("testA.d")
 
         val output = mSDialRestProcessor.process(input)
+        logger.warn(s"OUTPUT: ${output}")
+
+        output shouldBe a[File]
+        output.getName shouldEqual "testA.msdial"
 
         val resultLines = Source.fromFile(output).getLines().toSeq
-
         resultLines.head.split("\t") should contain ("Name")
         resultLines.head.split("\t") should contain ("ScanAtLeft")
 
@@ -47,9 +76,9 @@ class MSDialRestProcessorTest extends WordSpec with LazyLogging with ShouldMatch
       }
 
       //fails currently with a 500 error, need to wait till diego is back from vacation to fix this
-      "process a .abf file" in {
+      "process a .abf file" ignore { // this should only recieve raw data files from now on
 
-        val input = new File(getClass.getResource("/test.abf").toURI)
+        val input = new File("testA.abf")
 
         val output = mSDialRestProcessor.process(input)
 
