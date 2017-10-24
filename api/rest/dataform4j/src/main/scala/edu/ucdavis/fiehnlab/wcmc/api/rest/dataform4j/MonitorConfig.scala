5a9579441a89f87d834dc29a0affaae9ecf7b615
Diego Pedrosa <linuxmant@gmail.com>
2017-10-24 15:57:08 -0700

    fixed DataFormer4j config, refactored conversion logic, passed tests

--- api/rest/dataform4j/src/main/scala/edu/ucdavis/fiehnlab/wcmc/api/rest/dataform4j/MonitorConfig.scala
+++ api/rest/dataform4j/src/main/scala/edu/ucdavis/fiehnlab/wcmc/api/rest/dataform4j/MonitorConfig.scala
@@ -1,9 +1,13 @@
 package edu.ucdavis.fiehnlab.wcmc.api.rest.dataform4j
 
-import org.springframework.context.annotation.{ComponentScan, Configuration}
+import edu.ucdavis.fiehnlab.wcmc.api.rest.fserv4j.FServ4jClient
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.context.annotation.{Bean, Configuration}
 
 @Configuration
-@ComponentScan
+@EnableAutoConfiguration(exclude = Array(classOf[DataSourceAutoConfiguration]))
 class MonitorConfig {
-
+  @Bean
+  def fserv4j: FServ4jClient = new FServ4jClient("testfserv.fiehnlab.ucdavis.edu")
 }
