90d175403e93f60091a56f2ec4f831d582f64fea
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2017-11-02 13:31:52 -0700

    fixed component scan issues

--- pipeline/schedule/api/src/main/scala/edu/ucdavis/fiehnlab/ms/carrot/core/schedule/ScheduleConfig.scala
+++ pipeline/schedule/api/src/main/scala/edu/ucdavis/fiehnlab/ms/carrot/core/schedule/ScheduleConfig.scala
@@ -1,12 +1,13 @@
 package edu.ucdavis.fiehnlab.ms.carrot.core.schedule
 
-import org.springframework.context.annotation.{ComponentScan, Configuration}
+import org.springframework.context.annotation.{Bean, ComponentScan, Configuration}
 
 /**
   * Created by wohlgemuth on 9/15/17.
   */
 @Configuration
-@ComponentScan
 class ScheduleConfig {
 
+  @Bean
+  def taskRunner:TaskRunner = new TaskRunner
 }
