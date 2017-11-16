377638e8387a580902fce17986f4b4fad654c362
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-11-16 13:42:15 -0800

    overhaul of the schedule job view with better file checking/handling in scheduler app

--- pipeline/apps/server/src/main/resources/static/scripts/app.js
+++ pipeline/apps/server/src/main/resources/static/scripts/app.js
@@ -92,6 +92,72 @@ angular.module('app', ['ngAnimate', 'ngRoute', 'ui.bootstrap', 'ngHandsontable']
 
 
         /**
+         * Table functions
+         */
+        $scope.resetTableRowHeaders = function() {
+            var instance = hotRegisterer.getInstance('scheduler');
+            var rowLabels = instance.getRowHeader();
+
+            $scope.data.forEach(function(x, i) {
+                rowLabels[i] = i + 1;
+            });
+
+            instance.updateSettings({rowHeaders: rowLabels});
+        };
+
+        $scope.resetTable = function() {
+            $scope.resetTableRowHeaders();
+            $scope.data = [[]];
+        };
+
+        $scope.sortTableByValidity = function(includeBadRows) {
+            includeBadRows = angular.isDefined(includeBadRows) ? includeBadRows : true;
+
+            var instance = hotRegisterer.getInstance('scheduler');
+            var rowLabels = instance.getRowHeader();
+
+            // Find valid and invalid samples
+            var goodRows = [], badRows = [];
+
+            $scope.data.forEach(function(x, i) {
+                if (angular.isString(rowLabels[i])) {
+                    if (rowLabels[i].indexOf("fa-check") > 0) {
+                        goodRows.push(x);
+                    } else if (rowLabels[i].indexOf('fa-times') > 0) {
+                        badRows.push(x);
+                    }
+                }
+            });
+
+            // Update data
+            $scope.data = includeBadRows ? goodRows.concat(badRows) : goodRows;
+
+            if ($scope.data.length == 0) {
+                $scope.data = [[]];
+            }
+
+            // Update row headers
+            for (var i = 0; i < goodRows.length + badRows.length; i++) {
+                if (i < goodRows.length) {
+                    rowLabels[i] = '<i class="fa fa-check text-success" aria-hidden="true"></i>';
+                } else if (includeBadRows && i < goodRows.length + badRows.length) {
+                    rowLabels[i] = '<i class="fa fa-times text-danger" aria-hidden="true"></i>';
+                } else {
+                    rowLabels[i] = i + 1;
+                }
+            }
+
+            instance.updateSettings({rowHeaders: rowLabels});
+        };
+
+        $scope.removeInvalidRows = function() {
+            $scope.sortTableByValidity(false);
+            $scope.checkFileError = undefined
+            $scope.checkFileSuccess = $scope.taskToSubmit.samples.length > 0;
+        };
+
+
+        /**
          * Update HandsOnTable with selected column name
          */
         $scope.updateColumnName = function(idx) {
@@ -104,9 +170,11 @@ angular.module('app', ['ngAnimate', 'ngRoute', 'ui.bootstrap', 'ngHandsontable']
         };
 
 
-        $scope.submit = function() {
+        $scope.checkFiles = function() {
             // Reset error
             $scope.success = false;
+            $scope.checkFileSuccess = undefined;
+            $scope.checkFileError = undefined;
             $scope.error = undefined;
 
             // Check that filename column is selected
@@ -126,49 +194,26 @@ angular.module('app', ['ngAnimate', 'ngRoute', 'ui.bootstrap', 'ngHandsontable']
                 return;
             }
 
-            if (angular.isUndefined($scope.task.email)) {
-                $scope.error = 'No email address provided!';
-                return;
-            }
-            if (angular.isUndefined($scope.task.platform)) {
-                $scope.error = 'No platform selected!';
-                return;
-            }
-            if (angular.isUndefined($scope.task.acquisitionMethod)) {
-                $scope.error = 'No acquisition method selected!';
-                return;
-            }
-
-            $scope.running = true;
+            $scope.checkingFiles = true;
+            $scope.resetTableRowHeaders();
 
             // Task object to submit
-            var task = {
-                samples: [],
-                name: $scope.task.email,
-                email: $scope.task.email,
-                acquisitionMethod: $scope.task.acquisitionMethod,
-                platform: {platform: {name: $scope.task.platform}}
+            $scope.taskToSubmit = {
+                samples: []
             };
-
-            // Add task name
-            if (angular.isDefined($scope.task.name) && $scope.task.name != '') {
-                task.name += '_'+ $scope.task.name;
-            } else {
-                task.name += '_'+ $filter('date')(new Date(), 'yyyyMMddHHmmss');
-            }
 
             // Check file existence for each row and update the row header with the result
             var rowLabels = instance.getRowHeader();
             var checkCount = 0;
             var validCount = 0;
+            var errorCount = 0;
 
             $scope.data.forEach(function(x, i) {
-
                 if (x[fileNameCol] !== null && x[fileNameCol] !== "") {
                     // Replace extension if desired
                     if (angular.isDefined($scope.task.extension) && $scope.task.extension != "") {
                         if (x[fileNameCol].indexOf('.') > -1) {
-                            x[fileNameCol] = x[fileNameCol].substr(0, x[fileNameCol].lastIndexOf('.'));
+                            x[fileNameCol] = x[fileNameCol].substr(0, x[fileNameCol].lastIndexOf('.')) +'.'+ $scope.task.extension;
                         } else if (x[fileNameCol] != "") {
                             x[fileNameCol] += '.'+ $scope.task.extension;
                         }
@@ -194,7 +239,7 @@ angular.module('app', ['ngAnimate', 'ngRoute', 'ui.bootstrap', 'ngHandsontable']
                             if (!angular.equals(matrix, {}))
                                 sample.matrix = matrix;
 
-                            task.samples.push(sample);
+                            $scope.taskToSubmit.samples.push(sample);
 
                             // Update row header
                             rowLabels[i] = '<i class="fa fa-check text-success" aria-hidden="true"></i>';
@@ -208,39 +253,79 @@ angular.module('app', ['ngAnimate', 'ngRoute', 'ui.bootstrap', 'ngHandsontable']
                             instance.updateSettings({rowHeaders: rowLabels});
 
                             checkCount++;
+                            errorCount++;
                         }
                     );
+                } else if (x.filter(function(x) { return x != null && x != ""; }).length == 0) {
+                    // Ignore empty rows
+                    rowLabels[i] = i + 1;
+                    instance.updateSettings({rowHeaders: rowLabels});
+                    checkCount++;
                 } else {
                     rowLabels[i] = '<i class="fa fa-times text-danger" aria-hidden="true"></i>';
                     instance.updateSettings({rowHeaders: rowLabels});
-
                     checkCount++;
                 }
             });
 
             // Submit the task, waiting until all checks are complete
-            var submitTask = function() {
+            var monitorFileCheck = function() {
                 if (checkCount < $scope.data.length) {
-                    $timeout(submitTask, 1000);
-                } else if (validCount > 0) {
-                    HttpService.submitJob(
-                        task,
-                        function(data) {
-                            $scope.success = data.data;
-                            $scope.running = false;
-                        },
-                        function(data) {
-                            $scope.error = data.data;
-                            $scope.running = false;
-                        }
-                    );
+                    $timeout(monitorFileCheck, 1000);
+                } else if (errorCount > 0) {
+                    $scope.checkFileError = errorCount +"/"+ (validCount + errorCount) +" sample files could not be found!  Please remove or rename these."
+                    $scope.checkingFiles = false;
                 } else {
-                    $scope.error = 'No valid sample files were provided!';
-                    $scope.running = false;
+                    $scope.checkFileSuccess = true;
+                    $scope.checkingFiles = false;
                 }
             };
 
-            submitTask();
+            monitorFileCheck();
+        };
+
+        $scope.submit = function() {
+            if ($scope.checkFileError) {
+                $scope.error = 'Not all sample files are valid - please re-check files before submitting!';
+                return;
+            }
+            if (angular.isUndefined($scope.task.email)) {
+                $scope.error = 'No email address provided!';
+                return;
+            }
+            if (angular.isUndefined($scope.task.platform)) {
+                $scope.error = 'No platform selected!';
+                return;
+            }
+            if (angular.isUndefined($scope.task.acquisitionMethod)) {
+                $scope.error = 'No acquisition method selected!';
+                return;
+            }
+
+            $scope.taskToSubmit.name = $scope.task.email;
+            $scope.taskToSubmit.email = $scope.task.email;
+            $scope.taskToSubmit.acquisitionMethod = $scope.task.acquisitionMethod;
+            $scope.taskToSubmit.platform = {platform: {name: $scope.task.platform}};
+
+            // Add task name
+            if (angular.isDefined($scope.task.name) && $scope.task.name != '') {
+                $scope.taskToSubmit.name += '_'+ $scope.task.name;
+            } else {
+                $scope.taskToSubmit.name += '_'+ $filter('date')(new Date(), 'yyyyMMddHHmmss');
+            }
+
+            // Submit the task, waiting until all checks are complete
+            HttpService.submitJob(
+                $scope.taskToSubmit,
+                function(data) {
+                    $scope.success = data.data;
+                    $scope.submitting = false;
+                },
+                function(data) {
+                    $scope.error = data.data;
+                    $scope.submitting = false;
+                }
+            );
         };
     }])
 
