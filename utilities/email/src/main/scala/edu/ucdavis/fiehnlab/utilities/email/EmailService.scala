455f5e6d6ae583d3f2c7c6d9ae4e59de20ae9be1
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2017-11-30 12:57:28 -0800

    finished email service and associated tests

--- utilities/email/src/main/scala/edu/ucdavis/fiehnlab/utilities/email/EmailService.scala
+++ utilities/email/src/main/scala/edu/ucdavis/fiehnlab/utilities/email/EmailService.scala
@@ -1,9 +1,14 @@
 package edu.ucdavis.fiehnlab.utilities.email
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.mail.javamail.JavaMailSenderImpl
+
 import java.io.File
+import java.util.Properties
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.{Bean, Configuration}
+import org.springframework.context.annotation.{Bean, Configuration, Profile}
 import org.springframework.mail.javamail.{JavaMailSender, MimeMessageHelper}
 
 @Configuration
@@ -12,6 +17,22 @@ class EmailServiceAutoConfiguration {
   @Bean
   def emailService: EmailService = new EmailService
 
+  @Bean
+  def emailSender(@Value("${email.host}") emailHost: String, @Value("${email.port}") emailPort: Integer, @Value("${email.username}") username: String, @Value("${email.pass}") password: String): JavaMailSenderImpl = {
+    val emailSender = new JavaMailSenderImpl
+    emailSender.setHost(emailHost)
+    emailSender.setPort(emailPort)
+    emailSender.setUsername(username)
+    emailSender.setPassword(password)
+    //emailSender.setDefaultEncoding("UTF_8");
+    val mailProps = new Properties()
+    mailProps.setProperty("mail.transport.protocol", "smtp")
+    mailProps.setProperty("mail.smtp.auth", "true")
+    mailProps.setProperty("mail.smtp.starttls.enable", "true")
+    mailProps.setProperty("mail.debug", "false")
+    emailSender.setJavaMailProperties(mailProps)
+    emailSender
+  }
 }
 
 /**
@@ -28,7 +49,7 @@ class EmailService {
 
       val message = sender.createMimeMessage()
 
-      val helper = new MimeMessageHelper(message)
+      val helper = new MimeMessageHelper(message,true)
       helper.setFrom(from)
       helper.setText(content)
       helper.setSubject(subject)
---
+++ utilities/email/src/test/scala/edu/ucdavis/fiehnlab/utilities/email/EmailServiceTest.scala
@@ -0,0 +1,96 @@
+package edu.ucdavis.fiehnlab.utilities.email
+
+import java.io.{File, PrintWriter}
+import java.util.Properties
+
+import ch.qos.logback.core.util.FileUtil
+import org.junit.runner.RunWith
+import org.scalatest.{BeforeAndAfter, ShouldMatchers, WordSpec}
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.mail.javamail.{JavaMailSender, JavaMailSenderImpl}
+import org.springframework.test.context.TestContextManager
+import org.springframework.test.context.junit4.SpringRunner
+import com.icegreen.greenmail.util.GreenMail
+import com.icegreen.greenmail.util.ServerSetupTest
+
+@RunWith(classOf[SpringRunner])
+@SpringBootTest
+class EmailServiceTest extends WordSpec with BeforeAndAfter with ShouldMatchers {
+
+  @Autowired
+  val emailService: EmailService = null
+
+  @Autowired
+  val emailSender: JavaMailSenderImpl = null
+
+  val testSmtp = new GreenMail(ServerSetupTest.SMTP)
+
+  new TestContextManager(this.getClass).prepareTestInstance(this)
+
+
+  "EmailServiceTest" should {
+
+    "prepare" must {
+
+
+      testSmtp.start
+
+      //don't forget to set the test port!
+      emailSender.setPort(3025)
+      emailSender.setHost("localhost")
+
+
+      "send without a attachment" in {
+        emailService.send("test@test.de", "test@test.com" :: List(), "none", "no subject", None)
+
+        val messages = testSmtp.getReceivedMessages
+
+        messages.length shouldBe( 1)
+        messages(0).getSubject.toString shouldBe("no subject")
+      }
+
+      "send with an attachment" in {
+        val file = File.createTempFile("test","test")
+
+        new PrintWriter(file) {
+          write("tada")
+          close()
+        }
+
+        emailService.send("test@test.de", "test@test.com" :: List(), "none", "no subject", Option(file))
+
+        val messages = testSmtp.getReceivedMessages
+
+        messages.length shouldBe( 1 + 1)
+        messages(1).getSubject.toString shouldBe("no subject")
+        messages(0).getSize should be < messages(1).getSize
+      }
+
+
+      "send several messages" in {
+        emailService.send("test@test.de", "test@test.com" :: "test@test.de" :: List(), "none", "no subject", None)
+
+        val messages = testSmtp.getReceivedMessages
+
+        messages.length shouldBe( 2 + 1 + 1)
+      }
+    }
+
+  }
+
+  override protected def before(fun: => Any): Unit = {
+    testSmtp.start()
+  }
+
+  override protected def after(fun: => Any): Unit = {
+    testSmtp.stop()
+  }
+}
+
+@SpringBootApplication(exclude = Array(classOf[DataSourceAutoConfiguration]))
+class EmailServiceTestConfig {
+
+}
