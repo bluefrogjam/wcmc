0d985475cb2ee7ccddd6097348ea82e3e713d02d
Diego Pedrosa <linuxmant@gmail.com>
2018-01-16 09:56:21 -0800

    first converter prototype done

--- cts3/ctsserver/src/main/scala/edu/ucdavis/fiehnlab/cts3/model/Hit.scala
+++ cts3/ctsserver/src/main/scala/edu/ucdavis/fiehnlab/cts3/model/Hit.scala
@@ -1,3 +1,18 @@
 package edu.ucdavis.fiehnlab.cts3.model
 
-class Hit(keyword: String, from: String, to: String, result: String, score: Float) {}
+/**
+  * Actual result for a unique combination of conversion parameters
+  *
+  * @param keyword dirty string to convert
+  * @param from    type of id to convert from
+  * @param to      type of id to convert to
+  * @param result  conversion result
+  * @param score   conversion score
+  *
+  * Created by diego on 01/12/2018
+  */
+class Hit(val keyword: String, val from: String, val to: String, val result: String, val score: Float) {
+  override def toString():String = {
+    s"Keyword: $keyword, From: $from, To: $to, Result: $result, Score: $score"
+  }
+}
