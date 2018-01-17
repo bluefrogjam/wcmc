4c48b7b0403d99057cde41febddb2fa194bc06b5
Diego Pedrosa <linuxmant@gail.com>
2018-01-17 10:17:29 -0800

    added swagger, loging and some comments

--- cts3/ctsserver/src/main/scala/edu/ucdavis/fiehnlab/cts3/CtsApp.scala
+++ cts3/ctsserver/src/main/scala/edu/ucdavis/fiehnlab/cts3/CtsApp.scala
@@ -2,10 +2,11 @@ package edu.ucdavis.fiehnlab.cts3
 
 import org.springframework.boot.SpringApplication
 
+/**
+  * Created by diego on 1/12/2018
+  **/
 object CtsApp extends App {
-
-    val app = new SpringApplication(classOf[Cts])
-    app.setWebEnvironment(true)
-    val context = app.run(args: _*)
-
+  val app = new SpringApplication(classOf[Cts])
+  app.setWebEnvironment(true)
+  val context = app.run(args: _*)
 }
