5290db8093952111a58c7779499858f4b731a73c
Diego Pedrosa <linuxmant@gmail.com>
2018-01-17 10:21:40 -0800

    added tests for ConversionController and fixed controller convert action

--- cts3/ctsserver/src/main/scala/edu/ucdavis/fiehnlab/cts3/server/ConversionController.scala
+++ cts3/ctsserver/src/main/scala/edu/ucdavis/fiehnlab/cts3/server/ConversionController.scala
@@ -1,12 +1,23 @@
 package edu.ucdavis.fiehnlab.cts3.server
 
+import com.typesafe.scalalogging.LazyLogging
+import edu.ucdavis.fiehnlab.cts3.api.conversion.CactusConverter
 import edu.ucdavis.fiehnlab.cts3.model.Hit
-import org.springframework.web.bind.annotation.{PathVariable, RequestMapping, RequestParam, RestController}
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation._
 
-@RestController(value = "/rest/convert")
-class ConversionController {
-  @RequestMapping(value = Array("/{from}/{to}/{keyword}"))
-  def convert(@PathVariable from: String, @PathVariable to: String, @PathVariable keyword: String): Hit = {
-    new Hit("", "", "", "", 0.0f)
+/**
+  * Created by diego on 1/12/2018
+  **/
+@RestController
+@RequestMapping(value = Array("/rest"))
+class ConversionController extends LazyLogging {
+  @Autowired
+  val converter: CactusConverter = null
+
+  @GetMapping(value = Array("/convert/{from}/{to}/{keyword}"))
+  def convert(@PathVariable from: String, @PathVariable to: String, @PathVariable keyword: String): Seq[Hit] = {
+    logger.info(s"Converting $from: $keyword to $to")
+    converter.convert(keyword, from, to)
   }
 }
