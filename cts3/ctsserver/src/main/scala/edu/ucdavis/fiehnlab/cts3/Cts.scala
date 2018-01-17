c3ea42c4095132932f25bce899f76a1d69f07a74
Diego Pedrosa <linuxmant@gail.com>
2018-01-17 10:17:29 -0800

    added swagger, loging and some comments

--- cts3/ctsserver/src/main/scala/edu/ucdavis/fiehnlab/cts3/Cts.scala
+++ cts3/ctsserver/src/main/scala/edu/ucdavis/fiehnlab/cts3/Cts.scala
@@ -3,8 +3,9 @@ package edu.ucdavis.fiehnlab.cts3
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.{EnableAutoConfiguration, SpringBootApplication}
 
+/**
+  * Created by diego on 1/12/2018
+  **/
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = Array(classOf[DataSourceAutoConfiguration]))
-class Cts {
-
-}
+class Cts
