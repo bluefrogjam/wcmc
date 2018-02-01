c16306cb4c760dddc3693d08d215939961f1939e
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2018-02-01 14:04:42 -0800

    additional scan search method and improvements to lcms utilities

--- pipeline/core/msdial/src/main/java/edu/ucdavis/fiehnlab/ms/carrot/core/msdial/utils/LCMSDataAccessUtility.java
+++ pipeline/core/msdial/src/main/java/edu/ucdavis/fiehnlab/ms/carrot/core/msdial/utils/LCMSDataAccessUtility.java
@@ -154,6 +154,30 @@ public class LCMSDataAccessUtility {
         return startIndex;
     }
 
+    /**
+     *
+     * @param targetRt
+     * @param spectrumList
+     * @return
+     */
+    private static int getRtStartIndex(double targetRt, List<Feature> spectrumList) {
+
+        int startIndex = 0, endIndex = spectrumList.size() - 1;
+        int counter = 0;
+
+        while (counter < 10) {
+            if (spectrumList.get(startIndex).retentionTimeInMinutes() <= targetRt && targetRt < spectrumList.get((startIndex + endIndex) / 2).retentionTimeInMinutes()) {
+                endIndex = (startIndex + endIndex) / 2;
+            } else if (spectrumList.get((startIndex + endIndex) / 2).retentionTimeInMinutes() <= targetRt && targetRt < spectrumList.get(endIndex).retentionTimeInMinutes()) {
+                startIndex = (startIndex + endIndex) / 2;
+            }
+
+            counter++;
+        }
+
+        return startIndex;
+    }
+
 
     public static PeakAreaBean getPeakAreaBean(PeakDetectionResult peakResult) {
         if (peakResult == null)
@@ -190,8 +214,8 @@ public class LCMSDataAccessUtility {
 //		pab.metaboliteName = "";
 //		pab.adductIonName = "";
 //		pab.libraryID = -1;
-//		pab.isotopeWeightNumber = -1;
-//		pab.isotopeParentPeakID = -1;
+		peak.isotopeWeightNumber = -1;
+		peak.isotopeParentPeakID = -1;
 //		pab.adductParent = -1;
 //		pab.rtSimilarityValue = -1;
 //		pab.isotopeSimilarityValue = -1;
@@ -205,7 +229,70 @@ public class LCMSDataAccessUtility {
         return peak;
     }
 
-    public static int getMS2DatapointNumber(int startPoint, int endPoint, float accurateMass, double tolerance, List<Feature> spectrumList, IonMode ionmode) {
+
+    /**
+     *
+     * @param spectrumList
+     * @param precursorMz
+     * @param productMz
+     * @param startRt
+     * @param endRt
+     * @param ionMode
+     * @param centroidedMS1Tolerance
+     * @param centroidMS2Tolerance
+     * @return
+     */
+    public static List<double[]> getMS2Peaklist(List<Feature> spectrumList, double precursorMz, double productMz,double startRt,
+                                                double endRt, IonMode ionMode, double centroidedMS1Tolerance, double centroidMS2Tolerance) {
+
+        List<double[]> peakList = new ArrayList<>();
+
+        int startRtIndex = getRtStartIndex(startRt, spectrumList);
+
+        for (int i = startRtIndex; i < spectrumList.size(); i++) {
+            if (spectrumList.get(i).retentionTimeInMinutes() < startRt)
+                continue;
+            if (spectrumList.get(i).retentionTimeInMinutes() > endRt)
+                break;
+
+            if (spectrumList.get(i) instanceof MSMSSpectra && spectrumList.get(i).ionMode().get() == ionMode) {
+                MSMSSpectra spectrum = (MSMSSpectra)spectrumList.get(i);
+                List<Ion> massSpectrum = TypeConverter.getJavaIonList(spectrum);
+
+                double sum = 0;
+
+                if (Math.abs(precursorMz - spectrum.precursorIon()) < centroidedMS1Tolerance && !massSpectrum.isEmpty()) {
+                    int startMsIndex = getMs2StartIndex(productMz - centroidMS2Tolerance, massSpectrum);
+
+                    for (int j = startMsIndex; j < massSpectrum.size(); j++) {
+                        if (massSpectrum.get(j).mass() < productMz - centroidMS2Tolerance) {
+                            continue;
+                        } else if (Math.abs(productMz - massSpectrum.get(j).mass()) <= centroidMS2Tolerance) {
+                            sum += massSpectrum.get(j).intensity();
+                        } else {
+                            break;
+                        }
+                    }
+                }
+
+                peakList.add(new double[] { i, spectrum.retentionTimeInMinutes(), 0, sum });
+            }
+        }
+
+        return peakList;
+    }
+
+    /**
+     *
+     * @param startPoint
+     * @param endPoint
+     * @param accurateMass
+     * @param tolerance
+     * @param spectrumList
+     * @param ionMode
+     * @return
+     */
+    public static int getMS2DatapointNumber(int startPoint, int endPoint, float accurateMass, double tolerance, List<Feature> spectrumList, IonMode ionMode) {
         double maxIntensity = Double.MIN_VALUE;
         int maxID = -1;
 
@@ -213,7 +300,7 @@ public class LCMSDataAccessUtility {
             startPoint = 0;
 
         for (int i = startPoint; i < endPoint; i++) {
-            if (spectrumList.get(i) instanceof MSMSSpectra && spectrumList.get(i).ionMode().get() == ionmode) {
+            if (spectrumList.get(i) instanceof MSMSSpectra && spectrumList.get(i).ionMode().get() == ionMode) {
                 MSMSSpectra spectrum = (MSMSSpectra)spectrumList.get(i);
 
                 if (Math.abs(accurateMass - spectrum.precursorIon()) <= tolerance) {
