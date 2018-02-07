19babdf2ef25fcb476089acfbaec522149d0b461
Diego Pedrosa <linuxmant@gmail.com>
2018-02-07 13:38:53 -0800

    added PreakDetection as a pr-processing class to the workflow

--- pipeline/core/workflow/src/main/scala/edu/ucdavis/fiehnlab/ms/carrot/core/workflow/sample/preprocessing/PeakDetection.scala
+++ pipeline/core/workflow/src/main/scala/edu/ucdavis/fiehnlab/ms/carrot/core/workflow/sample/preprocessing/PeakDetection.scala
@@ -3,26 +3,31 @@ package edu.ucdavis.fiehnlab.ms.carrot.core.workflow.sample.preprocessing
 import com.typesafe.scalalogging.LazyLogging
 import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.AcquisitionMethod
 import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.sample.Sample
-import edu.ucdavis.fiehnlab.ms.carrot.core.msdial.MSDialProcessing
+import edu.ucdavis.fiehnlab.ms.carrot.core.msdial.{MSDialProcessing, MSDialProcessingProperties}
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
 
 /**
   * Created by diego on 2/7/2018
   **/
+@Component
 class PeakDetection extends PreProcessor with LazyLogging {
+
+  @Autowired
+  val msdialProcessor: MSDialProcessing = null
+
+  @Autowired
+  val processingProperties: MSDialProcessingProperties = null
+
+  override def priortiy: Int = 10
+
   /**
     * actually processes the item (implementations in subclasses)
     *
     * @param item
     * @return
     */
-
-  @Autowired
-  val msdialProcessor: MSDialProcessing= null
-
-  override def priortiy: Int = 10
-
   override def doProcess(item: Sample, method: AcquisitionMethod): Sample = {
-    msdialProcessor.process()
+    msdialProcessor.process(item, processingProperties)
   }
 }
