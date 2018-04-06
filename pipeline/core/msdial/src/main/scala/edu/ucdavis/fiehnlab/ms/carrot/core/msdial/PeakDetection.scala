d750e6b94e6f37241f0028306c037906d30dd3b3
Diego Pedrosa <linuxmant@gmail.com>
2018-04-06 12:56:46 -0700

    added missing imports

--- pipeline/core/msdial/src/main/scala/edu/ucdavis/fiehnlab/ms/carrot/core/msdial/PeakDetection.scala
+++ pipeline/core/msdial/src/main/scala/edu/ucdavis/fiehnlab/ms/carrot/core/msdial/PeakDetection.scala
@@ -5,7 +5,7 @@ import edu.ucdavis.fiehnlab.ms.carrot.core.api.process.PreProcessor
 import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.AcquisitionMethod
 import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.sample.Sample
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.{Description, Profile}
+import org.springframework.context.annotation._
 import org.springframework.stereotype.Component
 
 /**
