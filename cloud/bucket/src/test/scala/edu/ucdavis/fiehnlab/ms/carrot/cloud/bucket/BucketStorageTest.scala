c10911009dee09cd214d47b7735067ecca3b7ae3
Gert Wohlgemuth <berlinguyinca@gmail.com>
2018-04-11 13:04:10 -0700

    finished S3 bucket storage

---
+++ cloud/bucket/src/test/scala/edu/ucdavis/fiehnlab/ms/carrot/cloud/bucket/BucketStorageTest.scala
@@ -0,0 +1,53 @@
+package edu.ucdavis.fiehnlab.ms.carrot.cloud.bucket
+
+import java.io.{File, RandomAccessFile}
+
+import edu.ucdavis.fiehnlab.loader.{ResourceLoader, ResourceStorage}
+import org.junit.runner.RunWith
+import org.scalatest.{ShouldMatchers, WordSpec}
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.{ActiveProfiles, TestContextManager}
+
+@SpringBootApplication(exclude = Array(classOf[DataSourceAutoConfiguration]))
+class BucketStorageTestApplication
+
+@RunWith(classOf[SpringJUnit4ClassRunner])
+@SpringBootTest
+@ActiveProfiles(Array("carrot.resource.store.bucket", "carrot.resource.loader.bucket"))
+class BucketStorageTest extends WordSpec with ShouldMatchers{
+
+  @Autowired
+  val storage: ResourceStorage = null
+
+  @Autowired
+  val loader: ResourceLoader = null
+
+  new TestContextManager(this.getClass).prepareTestInstance(this)
+
+  "BucketStorageTest" should {
+
+    "store" in {
+
+      for (x <- 1 to 10 by 3) {
+        val temp = File.createTempFile("temp", "x")
+        temp.deleteOnExit()
+
+        val f = new RandomAccessFile(temp, "rw")
+        f.setLength(1024 * 1024 * x)
+        f.close()
+
+        storage.store(temp)
+
+        loader.exists(temp.getName) should be(true)
+
+        storage.delete(temp.getName)
+
+        loader.exists(temp.getName) should be(false)
+      }
+    }
+  }
+}
