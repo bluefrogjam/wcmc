09301b54c1bc0918fae76de963c10c35538cf464
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2018-04-10 21:11:28 -0700

    working on AWS buckets and file handling

--- pipeline/cloud/bucket/src/test/scala/edu/ucdavis/fiehnlab/ms/carrot/cloud/bucket/BucketStorageTest.scala
+++ pipeline/cloud/bucket/src/test/scala/edu/ucdavis/fiehnlab/ms/carrot/cloud/bucket/BucketStorageTest.scala
@@ -1,40 +1,26 @@
-package edu.ucdavis.fiehnlab.ms.carrot.cloud.storage.result
-
-import java.util
+package edu.ucdavis.fiehnlab.ms.carrot.cloud.bucket
 
 import org.junit.runner.RunWith
 import org.scalatest.WordSpec
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.{ActiveProfiles, TestContextManager}
 
-
 @SpringBootApplication(exclude = Array(classOf[DataSourceAutoConfiguration]))
-class DynamoDBTest
+class BucketStorageTestApplication
 
 @RunWith(classOf[SpringJUnit4ClassRunner])
 @SpringBootTest
-@ActiveProfiles(Array("carrot.store.result.dynamo"))
-class DynamoSampleRepositoryTest extends WordSpec {
-
-  @Autowired
-  val dynamoSampleRepository: DynamoSampleRepository = null
+@ActiveProfiles(Array("carrot.resource.store.bucket"))
+class BucketStorageTest extends WordSpec {
 
   new TestContextManager(this.getClass).prepareTestInstance(this)
 
-  "DynamoSampleRepositoryTest" should {
+  "BucketStorageTest" should {
 
-    "create a new object" in {
-
-    }
-    "findAll" in {
-
-    }
-
-    "findByFileName" in {
+    "store" in {
 
     }
 
