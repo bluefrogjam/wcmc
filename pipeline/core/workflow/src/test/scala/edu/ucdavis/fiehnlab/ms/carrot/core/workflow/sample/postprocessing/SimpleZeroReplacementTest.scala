4b8f246843c63e4b5418a07289901ff1bdd32ab3
Diego Pedrosa <linuxmant@gmail.com>
2018-04-13 15:20:42 -0700

    AcquisitionMethods can't be None if sample requires peak picking and deconvolution

--- pipeline/core/workflow/src/test/scala/edu/ucdavis/fiehnlab/ms/carrot/core/workflow/sample/postprocessing/SimpleZeroReplacementTest.scala
+++ pipeline/core/workflow/src/test/scala/edu/ucdavis/fiehnlab/ms/carrot/core/workflow/sample/postprocessing/SimpleZeroReplacementTest.scala
@@ -3,9 +3,8 @@ package edu.ucdavis.fiehnlab.ms.carrot.core.workflow.sample.postprocessing
 import com.typesafe.scalalogging.LazyLogging
 import edu.ucdavis.fiehnlab.ms.carrot.core.TargetedWorkflowTestConfiguration
 import edu.ucdavis.fiehnlab.ms.carrot.core.api.io.SampleLoader
-import edu.ucdavis.fiehnlab.ms.carrot.core.api.process.PostProcessing
-import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.AcquisitionMethod
-import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.sample.QuantifiedSample
+import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.sample.{IonMode, QuantifiedSample}
+import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.{AcquisitionMethod, ChromatographicMethod}
 import edu.ucdavis.fiehnlab.ms.carrot.core.msdial.PeakDetection
 import edu.ucdavis.fiehnlab.ms.carrot.core.workflow.sample.annotation.LCMSTargetAnnotationProcess
 import edu.ucdavis.fiehnlab.ms.carrot.core.workflow.sample.correction.lcms.LCMSTargetRetentionIndexCorrectionProcess
@@ -14,13 +13,13 @@ import org.junit.runner.RunWith
 import org.scalatest.{ShouldMatchers, WordSpec}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.{ActiveProfiles, TestContextManager}
 
 /**
   * Created by wohlg on 7/13/2016.
   */
-@RunWith(classOf[SpringRunner])
+@RunWith(classOf[SpringJUnit4ClassRunner])
 @SpringBootTest(classes = Array(classOf[TargetedWorkflowTestConfiguration]))
 @ActiveProfiles(Array("carrot.report.quantify.height", "carrot.processing.replacement.simple", "carrot.processing.peakdetection", "carrot.lcms", "file.source.luna", "file.source.eclipse", "file.source.localhost"))
 class SimpleZeroReplacementTest extends WordSpec with LazyLogging with ShouldMatchers {
@@ -41,15 +40,12 @@ class SimpleZeroReplacementTest extends WordSpec with LazyLogging with ShouldMat
   val quantify: QuantifyByHeightProcess = null
 
   @Autowired
-  val postprocess: PostProcessing[Double] = null
-
-  @Autowired
   val loader: SampleLoader = null
 
   new TestContextManager(this.getClass).prepareTestInstance(this)
 
   "SimpleZeroReplacementTest" must {
-    val method = AcquisitionMethod(None)
+    val method = AcquisitionMethod(Some(ChromatographicMethod("replacement test", None, None, Some(new IonMode("positive")))))
 
     val sample: QuantifiedSample[Double] = quantify.process(
       annotation.process(
