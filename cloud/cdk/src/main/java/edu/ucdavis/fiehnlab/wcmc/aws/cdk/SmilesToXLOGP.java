5ebd8532db31d707cec9b89e03378cb57c4d8b51
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2018-04-16 17:15:45 -0700

    working on fingerprint and log2p lambdas

--- cloud/cdk/src/main/java/edu/ucdavis/fiehnlab/wcmc/aws/cdk/SmilesToXLOGP.java
+++ cloud/cdk/src/main/java/edu/ucdavis/fiehnlab/wcmc/aws/cdk/SmilesToXLOGP.java
@@ -9,21 +9,36 @@ import org.openscience.cdk.qsar.DescriptorValue;
 import org.openscience.cdk.qsar.descriptors.molecular.XLogPDescriptor;
 import org.openscience.cdk.qsar.result.DoubleResult;
 import org.openscience.cdk.smiles.SmilesParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class SmilesToXLOGP implements RequestHandler<String, Double> {
-    SmilesParser parser = new SmilesParser(DefaultChemObjectBuilder.getInstance());
-    XLogPDescriptor descriptor = new XLogPDescriptor();
+import java.util.HashMap;
+import java.util.Map;
 
+public class SmilesToXLOGP extends ApiGatewayHandler {
+    private SmilesParser parser = new SmilesParser(DefaultChemObjectBuilder.getInstance());
+    private XLogPDescriptor descriptor = new XLogPDescriptor();
 
-    @Override
-    public Double handleRequest(String smile, Context context) {
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
+    /**
+     * evaluates the request from the client
+     *
+     * @param request
+     * @return
+     * @throws InvalidSmilesException
+     */
+    protected Map<String, Object> evaluateRequest(Map<String, Object> request) {
         try {
+            String smile = request.get("body").toString();
+
             IAtomContainer container = parser.parseSmiles(smile);
             DescriptorValue value = descriptor.calculate(container);
-            return ((DoubleResult) value.getValue()).doubleValue();
+            Map<String, Object> result = new HashMap<>();
 
-        } catch (InvalidSmilesException e) {
+            result.put("result", ((DoubleResult) value.getValue()).doubleValue());
+            return result;
+        } catch (Exception e) {
             throw new RuntimeException(e.getMessage(), e);
         }
     }
