28f07a8bea091ee45c9f12c2a848b6eddcd3694e
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2018-04-23 23:15:56 -0700

    tracing down bug, which causes exceptions on reattemting to load mzXML files with everything

--- pipeline/core/io/sampleloader/src/test/scala/edu/ucdavis/fiehnlab/ms/carrot/core/io/ConversionAwareSampleLoaderTest.scala
+++ pipeline/core/io/sampleloader/src/test/scala/edu/ucdavis/fiehnlab/ms/carrot/core/io/ConversionAwareSampleLoaderTest.scala
@@ -1,5 +1,6 @@
 package edu.ucdavis.fiehnlab.ms.carrot.core.io
 
+import edu.ucdavis.fiehnlab.loader.ResourceLoader
 import edu.ucdavis.fiehnlab.ms.carrot.core.api.io.SampleLoader
 import edu.ucdavis.fiehnlab.wcmc.api.rest.dataform4j.DataFormerClient
 import edu.ucdavis.fiehnlab.wcmc.api.rest.everything4j.Everything4J
@@ -9,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.{Bean, Profile}
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.context.{ActiveProfiles, TestContextManager}
 
@@ -42,10 +43,7 @@ class ConversionAwareSampleLoaderTestConfiguration {
   @Autowired
   val dataForm: DataFormerClient = null
 
-  @Autowired
-  val everything4Jluna: Everything4J = null
-
   @Bean
-  def loader: ConversionAwareSampleLoader = new ConversionAwareSampleLoader(dataForm, everything4Jluna)
+  def loader(resourceLoader:ResourceLoader): ConversionAwareSampleLoader = new ConversionAwareSampleLoader(dataForm, resourceLoader)
 
 }
