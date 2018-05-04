4d849a63b3f7212aa9bdc6c5c99060d3cab6ffd9
Diego Pedrosa <linuxmant@gmail.com>
2018-05-04 13:24:06 -0700

    Refactor FileWriter to GzipOutputStream on the msdial serializer

--- pipeline/core/msdial/src/main/scala/edu/ucdavis/fiehnlab/ms/carrot/core/msdial/utils/SampleSerializer.scala
+++ pipeline/core/msdial/src/main/scala/edu/ucdavis/fiehnlab/ms/carrot/core/msdial/utils/SampleSerializer.scala
@@ -1,20 +1,19 @@
 package edu.ucdavis.fiehnlab.ms.carrot.core.msdial.utils
 
-import java.io.{File, FileWriter}
+import java.io.{File, FileOutputStream, FileWriter}
+import java.util.zip.GZIPOutputStream
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.typesafe.scalalogging.LazyLogging
-import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.sample.{Ion, Sample}
 import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.sample.ms.{Feature, MSSpectra, MetadataSupport}
-import edu.ucdavis.fiehnlab.ms.carrot.core.msdial.types.MSDialLCMSProcessedSample
-import org.springframework.beans.factory.annotation.{Autowired, Value}
+import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.sample.{Ion, Sample}
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
 class SampleSerializer() extends LazyLogging {
 
-  @Value("${carrot.io.sample.destination:./}")
-  val destination = ""
+  val destination: String = System.getProperty("user.home") + "/.carrot_storage/deconvolution"
 
   @Autowired
   val objectMapper: ObjectMapper = null
@@ -37,14 +36,21 @@ class SampleSerializer() extends LazyLogging {
 
 
   def saveFile(sample: Sample): Unit = {
-    val file = new File(destination, s"${sample.name}_deco.json")
-    val writer: FileWriter = new FileWriter(file)
+    val file = new File(destination, s"${sample.name}_deco.csv.gz")
+    if (!file.getParentFile.exists()) {
+      file.getParentFile.mkdirs
+    }
+    println(s"Saving deconvolution data to: ${file.getAbsolutePath}")
 
-    writer.append("sample filename,").append("scan number,").append("ion mode,").append("mass of detected feature mz,").append("mass of detected feature int,").append("purity,").append("rt(s),").append("signal noise,").append("unique mass,").append("metadata").append("\n")
+    val writer: StringBuilder = new StringBuilder()
+    val gzos: GZIPOutputStream = new GZIPOutputStream(new FileOutputStream(file), true)
+
+    gzos.write(writer.append("sample filename,").append("scan number,").append("ion mode,").append("mass of detected feature mz,").append("mass of detected feature int,").append("purity,").append("rt(s),").append("signal noise,").append("unique mass,").append("metadata").append("\n").toString.getBytes)
+    writer.clear()
 
     sample.spectra.foreach {
       case spec: Feature with MetadataSupport =>
-        writer.append(s"${sample.fileName},")
+        gzos.write(writer.append(s"${sample.fileName},")
             .append(s"${spec.scanNumber},")
             .append(s"${spec.ionMode},")
             .append(s"${spec.massOfDetectedFeature.get.mass},")
@@ -54,9 +60,10 @@ class SampleSerializer() extends LazyLogging {
             .append(s"${spec.uniqueMass},")
             .append(s"${spec.metadata.map(x => s"${x._1}=${x._2}").mkString(";")},")
             .append(s"${spec.associatedScan.get.ions.map(ion => s"${ion.mass}:${ion.intensity}").mkString(" ")}")
-            .append(s"\n")
+            .append(s"\n").toString.getBytes)
+        writer.clear()
       case msspec: MSSpectra  =>
-        writer.append(s"${sample.fileName},")
+        gzos.write(writer.append(s"${sample.fileName},")
             .append(s"${msspec.scanNumber},")
             .append(s"${msspec.ionMode},")
             .append(s"${msspec.massOfDetectedFeature.getOrElse(Ion(0,0)).mass},")
@@ -66,12 +73,12 @@ class SampleSerializer() extends LazyLogging {
             .append(s"${msspec.uniqueMass},")
             .append(s"no metadata,")
             .append(s"${msspec.associatedScan.get.ions.map(ion => s"${ion.mass}:${ion.intensity}").mkString(" ")}")
-            .append(s"\n")
+            .append(s"\n").toString.getBytes)
+        writer.clear()
     }
 
-    writer.flush()
-    writer.close()
-    logger.info(s"... finished.")
+    gzos.flush()
+    gzos.close()
   }
 
 }
