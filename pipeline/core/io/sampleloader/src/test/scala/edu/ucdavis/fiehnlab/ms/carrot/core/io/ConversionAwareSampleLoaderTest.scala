6b428471148021d906facbb448049df03d3b4857
Diego Pedrosa <linuxmant@gmail.com>
2018-06-21 14:59:01 -0700

    fixed missing stasis.baseurl property for tests

--- pipeline/core/io/sampleloader/src/test/scala/edu/ucdavis/fiehnlab/ms/carrot/core/io/ConversionAwareSampleLoaderTest.scala
+++ pipeline/core/io/sampleloader/src/test/scala/edu/ucdavis/fiehnlab/ms/carrot/core/io/ConversionAwareSampleLoaderTest.scala
@@ -1,22 +1,16 @@
 package edu.ucdavis.fiehnlab.ms.carrot.core.io
 
-import edu.ucdavis.fiehnlab.loader.ResourceLoader
 import edu.ucdavis.fiehnlab.ms.carrot.core.api.io.SampleLoader
-import edu.ucdavis.fiehnlab.wcmc.api.rest.dataform4j.DataFormerClient
-import edu.ucdavis.fiehnlab.wcmc.api.rest.everything4j.Everything4J
 import org.junit.runner.RunWith
 import org.scalatest.{ShouldMatchers, WordSpec}
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.{Bean, Profile}
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.context.{ActiveProfiles, TestContextManager}
 
 @RunWith(classOf[SpringRunner])
-@SpringBootTest
 @ActiveProfiles(Array("file.source.luna"))
+@SpringBootTest(classes = Array(classOf[SampleLoaderTestConfig]))
 class ConversionAwareSampleLoaderTest extends WordSpec with ShouldMatchers {
 
   @Autowired
@@ -35,15 +29,4 @@ class ConversionAwareSampleLoaderTest extends WordSpec with ShouldMatchers {
       sample.get.fileName === name
     }
   }
-}
-
-@SpringBootApplication(exclude = Array(classOf[DataSourceAutoConfiguration]))
-class ConversionAwareSampleLoaderTestConfiguration {
-
-  @Autowired
-  val dataForm: DataFormerClient = null
-
-  @Bean
-  def loader(resourceLoader:ResourceLoader): ConversionAwareSampleLoader = new ConversionAwareSampleLoader(dataForm, resourceLoader)
-
 }
