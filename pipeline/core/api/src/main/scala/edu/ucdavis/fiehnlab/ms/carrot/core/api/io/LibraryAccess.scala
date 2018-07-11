da90cb0b5937fb91caf799b389566f422f86cd6e
Gert Wohlgemuth <berlinguyinca@gmail.com>
2018-07-11 13:03:32 -0700

    removing delegate autowiring

--- pipeline/core/api/src/main/scala/edu/ucdavis/fiehnlab/ms/carrot/core/api/io/LibraryAccess.scala
+++ pipeline/core/api/src/main/scala/edu/ucdavis/fiehnlab/ms/carrot/core/api/io/LibraryAccess.scala
@@ -109,8 +109,6 @@ trait ReadonlyLibrary[T <: Target] extends LibraryAccess[T] {
 
 }
 
-@Component
-@Primary
 class DelegateLibraryAccess[T <: Target] @Autowired()(delegates: java.util.List[LibraryAccess[T]]) extends LibraryAccess[T] {
 
   /**
