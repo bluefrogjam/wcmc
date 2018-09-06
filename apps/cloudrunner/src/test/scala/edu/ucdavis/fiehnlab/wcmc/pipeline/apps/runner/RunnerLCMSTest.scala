cfa9213e3a9d0db90f98a8a810172a762ca672a9
Diego Pedrosa <linuxmant@gmail.com>
2018-09-06 14:49:41 -0700

    refactored test

--- apps/cloudrunner/src/test/scala/edu/ucdavis/fiehnlab/wcmc/pipeline/apps/runner/RunnerLCMSTest.scala
+++ apps/cloudrunner/src/test/scala/edu/ucdavis/fiehnlab/wcmc/pipeline/apps/runner/RunnerLCMSTest.scala
@@ -1,7 +1,7 @@
 package edu.ucdavis.fiehnlab.wcmc.pipeline.apps.runner
 
 import com.typesafe.scalalogging.LazyLogging
-import edu.ucdavis.fiehnlab.wcmc.api.rest.stasis4j.client.StasisClient
+import edu.ucdavis.fiehnlab.wcmc.api.rest.stasis4j.api.StasisService
 import org.junit.runner.RunWith
 import org.scalatest.{ShouldMatchers, WordSpec}
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,7 +11,8 @@ import org.springframework.test.context.{ActiveProfiles, TestContextManager, Tes
 
 @RunWith(classOf[SpringRunner])
 @SpringBootTest(classes = Array(classOf[RunnerTestConfig]))
-@ActiveProfiles(Array("carrot.lcms", "carrot.output.writer.aws", "test", "file.source.eclipse"))
+@ActiveProfiles(Array("test", "carrot.lcms",
+  "carrot.output.writer.aws", "file.source.eclipse"))
 @TestPropertySource(properties = Array(
   "CARROT_SAMPLE:B5_P20Lipids_Pos_NIST01.mzML",
   "CARROT_METHOD:lcms_istds | test | test | positive",
@@ -24,19 +25,21 @@ class RunnerLCMSTest extends WordSpec with ShouldMatchers with LazyLogging {
   val runner: Runner = null
 
   @Autowired
-  val stasis_cli: StasisClient = null
+  val stasis_cli: StasisService = null
 
   new TestContextManager(this.getClass).prepareTestInstance(this)
 
-  " a runner" should {
-    "load the required sample and" must {
-      "process it" in {
-        runner.run()
-      }
+  "a runner" should {
+    "have a stasis client" in {
+      stasis_cli should not be null
+    }
 
-      "have results on aws" in {
-        stasis_cli.getResults("B5_P20Lipids_Pos_NIST01") should not be null
-      }
+    "process a sample" in {
+      runner.run()
+    }
+
+    "have results on aws" in {
+      stasis_cli.getResults("B5_P20Lipids_Pos_NIST01") should not be null
     }
   }
 }
