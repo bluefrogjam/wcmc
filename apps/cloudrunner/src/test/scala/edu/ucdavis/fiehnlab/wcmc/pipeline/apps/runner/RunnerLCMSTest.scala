bfde9fcfb066c96142cd37a1ccd8ac486785ed3b
Diego Pedrosa <linuxmant@gmail.com>
2018-09-27 15:48:23 -0700

    fixed properties on CloudRunner and configuration on tests

--- apps/cloudrunner/src/test/scala/edu/ucdavis/fiehnlab/wcmc/pipeline/apps/runner/RunnerLCMSTest.scala
+++ apps/cloudrunner/src/test/scala/edu/ucdavis/fiehnlab/wcmc/pipeline/apps/runner/RunnerLCMSTest.scala
@@ -4,15 +4,16 @@ import com.typesafe.scalalogging.LazyLogging
 import edu.ucdavis.fiehnlab.wcmc.api.rest.stasis4j.api.StasisService
 import org.junit.runner.RunWith
 import org.scalatest.{ShouldMatchers, WordSpec}
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.{Autowired, Value}
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.context.{ActiveProfiles, TestContextManager, TestPropertySource}
+import org.springframework.web.client.HttpClientErrorException
 
 @RunWith(classOf[SpringRunner])
-@SpringBootTest /*(classes = Array(classOf[RunnerTestConfig]))*/
-@ActiveProfiles(Array("test", "carrot.lcms",
-  "carrot.output.writer.aws", "file.source.eclipse"))
+@SpringBootTest
+@ActiveProfiles(Array("test", "carrot.lcms", "carrot.output.storage.aws", "carrot.scheduler.required",
+  "file.source.eclipse", "file.source.luna"))
 @TestPropertySource(properties = Array(
   "CARROT_SAMPLE:B5_P20Lipids_Pos_NIST01.mzML",
   "CARROT_METHOD:lcms_istds | test | test | positive",
@@ -20,6 +21,8 @@ import org.springframework.test.context.{ActiveProfiles, TestContextManager, Tes
   "carrot.submitter:dpedrosa@ucdavis.edu"
 ))
 class RunnerLCMSTest extends WordSpec with ShouldMatchers with LazyLogging {
+  @Value("${carrot.sample:#{environment.CARROT_SAMPLE}}")
+  val sampleName = ""
 
   @Autowired
   val runner: Runner = null
@@ -39,7 +42,12 @@ class RunnerLCMSTest extends WordSpec with ShouldMatchers with LazyLogging {
     }
 
     "have results on aws" in {
-      stasis_cli.getResults("B5_P20Lipids_Pos_NIST01") should not be null
+      try {
+        stasis_cli.getResults(sampleName.split('.')(0)) should not be null
+      } catch {
+        case ex: HttpClientErrorException =>
+          fail(ex)
+      }
     }
   }
 }
