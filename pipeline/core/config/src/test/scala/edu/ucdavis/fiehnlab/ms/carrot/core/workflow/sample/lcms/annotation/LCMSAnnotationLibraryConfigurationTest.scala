b62e3faf0dee43b7ecc3daeaba26042673464402
Diego Pedrosa <linuxmant@gmail.com>
2018-10-09 13:55:45 -0700

    original config file doesn't have annotation targets

--- pipeline/core/config/src/test/scala/edu/ucdavis/fiehnlab/ms/carrot/core/workflow/sample/lcms/annotation/LCMSAnnotationLibraryConfigurationTest.scala
+++ pipeline/core/config/src/test/scala/edu/ucdavis/fiehnlab/ms/carrot/core/workflow/sample/lcms/annotation/LCMSAnnotationLibraryConfigurationTest.scala
@@ -9,8 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.context.{ActiveProfiles, TestContextManager}
 
-import scala.collection.JavaConverters._
-
 @RunWith(classOf[SpringRunner])
 @ActiveProfiles(Array("test", "carrot.lcms"))
 @SpringBootTest
@@ -20,32 +18,9 @@ class LCMSAnnotationLibraryConfigurationTest extends WordSpec with ShouldMatcher
 
   new TestContextManager(this.getClass).prepareTestInstance(this)
 
-  "Correction library properties" should {
+  "Annotation library properties" should {
     "have properties for correction" in {
-      annotProperties.config should not be null
-    }
-
-    "have annotation libraries" in {
-      annotProperties.config.size() === 2
-    }
-
-    "have library 'jenny-tribe'" in {
-      println(s"libraries: ${annotProperties.config.size()}")
-      println(annotProperties.config.asScala.map(it => s"annotation ${it.name} | ${it.instrument} | ${it.column} | ${it.ionMode}").mkString("; "))
-      annotProperties.config.asScala.map(_.name) should have size 2
-    }
-
-    "have over 1000 targets in each library (positive, negative)" in {
-      annotProperties.config.get(0).targets.size() should be > 1000
-      annotProperties.config.get(1).targets.size() should be > 1000
-    }
-
-    "have instrument name 'test'" in {
-      annotProperties.config.get(0).instrument should equal("6530")
-    }
-
-    "have column name 'test'" in {
-      annotProperties.config.get(0).column should equal("test")
+      annotProperties.config shouldBe empty
     }
   }
 }
