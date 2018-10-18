8d42103875f96f39498ae36b9e7cd365c996e9d2
Diego Pedrosa <linuxmant@gmail.com>
2018-10-18 10:07:06 -0700

    finihshing local runner

--- apps/localrunner/src/main/scala/edu/ucdavis/fiehnlab/wcmc/apps/localrunner/LocalRunner.scala
+++ apps/localrunner/src/main/scala/edu/ucdavis/fiehnlab/wcmc/apps/localrunner/LocalRunner.scala
@@ -4,13 +4,16 @@ import java.io.FileNotFoundException
 
 import com.typesafe.scalalogging.LazyLogging
 import edu.ucdavis.fiehnlab.ms.carrot.core.api.io.{DelegateLibraryAccess, LibraryAccess, MergeLibraryAccess}
-import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.sample.{AnnotationTarget, CorrectionTarget}
-import edu.ucdavis.fiehnlab.ms.carrot.core.schedule.ScheduleConfig
+import edu.ucdavis.fiehnlab.ms.carrot.core.api.storage.{SampleToProcess, Task}
+import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.sample.{AnnotationTarget, CorrectionTarget, PositiveMode}
+import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.{AcquisitionMethod, ChromatographicMethod, Matrix}
+import edu.ucdavis.fiehnlab.ms.carrot.core.schedule.TaskRunner
 import edu.ucdavis.fiehnlab.ms.carrot.core.workflow.Workflow
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot._
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.{Bean, ComponentScan, Configuration}
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.context.annotation.{Bean, Configuration}
 
 import scala.io.Source
 
@@ -20,15 +23,20 @@ object LocalRunner extends App {
   val context = app.run(args: _*)
 }
 
-@SpringBootApplication(exclude = Array(classOf[ScheduleConfig]))
+@SpringBootApplication(exclude = Array(classOf[DataSourceAutoConfiguration]))
 class LocalRunner extends CommandLineRunner with LazyLogging {
   @Autowired
   val workflow: Workflow[Double] = null
 
+  @Autowired
+  val taskRunner: TaskRunner = null
+
   override def run(args: String*): Unit = {
     if (args.size != 2) {
-      logger.error("Please provide a sample list file and an acquisition method name.")
+      logger.error("\nPlease provide a sample list file and an acquisition method name.\n")
       System.exit(0)
+    } else {
+      logger.info(s"Arguments: ${args.mkString("\n")}")
     }
 
     val method = args(1)
@@ -47,40 +55,40 @@ class LocalRunner extends CommandLineRunner with LazyLogging {
         System.exit(-1)
       case ex: Throwable => logger.error(s"Somethig bad happened: ${ex.getMessage}")
     }
+
+    System.exit(0)
   }
 
   def process(fileList: Seq[String], method: String): Unit = {
     fileList.foreach { sample =>
       logger.info(s"Processing sample: ${sample}")
-      //      val task = Task(s"${sample} processing",
-      //        "linuxmant@gmail.com",
-      //        AcquisitionMethod(
-      //          ChromatographicMethod("jenny-tribe", Some("6530"), Some("test"), Some(PositiveMode()))
-      //        ),
-      //        Seq(SampleToProcess(sample, "", "", sample,
-      //          Matrix(System.currentTimeMillis().toString, "human", "plasma", Seq.empty)
-      //        )),
-      //        mode = "lcms",
-      //        env = "prod"
-      //      )
-      //
-      //      try {
-      //        val start = System.currentTimeMillis()
-      //        taskRunner.run(task)
-      //        println()
-      //        logger.info(s"\tSuccessfully finished processing ${sample} in ${(System.currentTimeMillis() - start) / 1000} s")
-      //        println()
-      //
-      //      } catch {
-      //        case ex: Exception =>
-      //          logger.error(s"\tFailed processing ${sample}.", ex)
-      //      }
+      val task = Task(s"${sample} processing",
+        "linuxmant@gmail.com",
+        AcquisitionMethod(
+          ChromatographicMethod("jenny-tribe", Some("6530"), Some("test"), Some(PositiveMode()))
+        ),
+        Seq(SampleToProcess(sample, "", "", sample,
+          Matrix(System.currentTimeMillis().toString, "human", "plasma", Seq.empty)
+        )),
+        mode = "lcms",
+        env = "prod"
+      )
+      try {
+        val start = System.currentTimeMillis()
+        taskRunner.run(task)
+        println()
+        logger.info(s"\tSuccessfully finished processing ${sample} in ${(System.currentTimeMillis() - start) / 1000} s")
+        println()
+
+      } catch {
+        case ex: Exception =>
+          logger.error(s"\tFailed processing ${sample}.", ex)
+      }
     }
   }
 }
 
 @Configuration
-@ComponentScan
 class LocalRunnerConfiguration extends LazyLogging {
   @Bean
   def workflow: Workflow[Double] = new Workflow[Double]()
