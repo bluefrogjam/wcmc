bb5865e283619233e67952b0ce08e278a2732d36
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2019-09-16 12:23:18 -0400

    adding ms-dial metadata to features with associated ms/ms scans

--- pipeline/core/msdial/src/main/scala/edu/ucdavis/fiehnlab/ms/carrot/core/msdial/types/MSDialLCMSProcessedSample.scala
+++ pipeline/core/msdial/src/main/scala/edu/ucdavis/fiehnlab/ms/carrot/core/msdial/types/MSDialLCMSProcessedSample.scala
@@ -1,8 +1,5 @@
 package edu.ucdavis.fiehnlab.ms.carrot.core.msdial.types
 
-import java.util
-
-import org.apache.logging.log4j.scala.Logging
 import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.sample._
 import edu.ucdavis.fiehnlab.ms.carrot.core.api.types.sample.ms._
 import edu.ucdavis.fiehnlab.ms.carrot.core.msdial.types.lcms.MS2DeconvolutionResult
@@ -32,9 +29,9 @@ object MSDialLCMSProcessedSample {
     }
   }
 
-  def generateSpectra(ms2DecResults: java.util.List[MS2DeconvolutionResult], fileName: String, mode: IonMode) = {
-    ms2DecResults.asScala.map { x: MS2DeconvolutionResult =>
+  def generateSpectra(ms2DecResults: java.util.List[MS2DeconvolutionResult], fileName: String, mode: IonMode): Seq[MSSpectra] = {
 
+    ms2DecResults.asScala.map { x: MS2DeconvolutionResult =>
       val _fileName = fileName
       val _scanNummer = x.peakTopScan
       val _retentionTimeInSecods = x.peakTopRetentionTime
@@ -45,31 +42,30 @@ object MSDialLCMSProcessedSample {
         override val ions: Seq[Ion] = x.ms1Spectrum.asScala
       })
 
+      val _metadata = Map(
+        "baseChromatogram" -> Some(x.baseChromatogram.asScala),
+        "modelMasses" -> Some(x.modelMasses.asScala),
+        "ms1AccurateMass" -> Some(x.ms1AccurateMass),
+        "mPlus1Height" -> Some(x.ms1IsotopicIonM1PeakHeight),
+        "mPlus2Height" -> Some(x.ms1IsotopicIonM2PeakHeight),
+        "peakHeight" -> Some(x.ms1PeakHeight),
+        "peakRTmin" -> Some(x.peakTopRetentionTime),
+        "uniqueMass" -> Some(x.uniqueMs),
+        "peak" -> Map[String, AnyRef](
+          "id" -> Some(x.peak.peakID),
+          "accurateMass" -> Some(x.peak.accurateMass),
+          "amplitudeOrderValue" -> Some(x.peak.amplitudeOrderValue),
+          "amplitudeScoreValue" -> Some(x.peak.amplitudeScoreValue),
+          "areaAboveBaseline" -> Some(x.peak.areaAboveBaseline),
+          "areaAboveZero" -> Some(x.peak.areaAboveZero),
+          "basePeakValue" -> Some(x.peak.basePeakValue),
+          "charge" -> Some(x.peak.chargeNumber),
+          "gaussianSimilarityValue" -> Some(x.peak.gaussianSimilarityValue),
+          "idealSlopeValue" -> Some(x.peak.idealSlopeValue),
+          "normalizedValue" -> Some(x.peak.normalizedValue)
+        ))
+
       if (x.peak.ms2LevelDataPointNumber == -1) {
-
-        val _metadata = Map(
-          "baseChromatogram" -> Some(x.baseChromatogram.asScala),
-          "modelMasses" -> Some(x.modelMasses.asScala),
-          "ms1AccurateMass" -> Some(x.ms1AccurateMass),
-          "mPlus1Height" -> Some(x.ms1IsotopicIonM1PeakHeight),
-          "mPlus2Height" -> Some(x.ms1IsotopicIonM2PeakHeight),
-          "peakHeight" -> Some(x.ms1PeakHeight),
-          "peakRTmin" -> Some(x.peakTopRetentionTime),
-          "uniqueMass" -> Some(x.uniqueMs),
-          "peak" -> Map[String, AnyRef](
-            "id" -> Some(x.peak.peakID),
-            "accurateMass" -> Some(x.peak.accurateMass),
-            "amplitudeOrderValue" -> Some(x.peak.amplitudeOrderValue),
-            "amplitudeScoreValue" -> Some(x.peak.amplitudeScoreValue),
-            "areaAboveBaseline" -> Some(x.peak.areaAboveBaseline),
-            "areaAboveZero" -> Some(x.peak.areaAboveZero),
-            "basePeakValue" -> Some(x.peak.basePeakValue),
-            "charge" -> Some(x.peak.chargeNumber),
-            "gaussianSimilarityValue" -> Some(x.peak.gaussianSimilarityValue),
-            "idealSlopeValue" -> Some(x.peak.idealSlopeValue),
-            "normalizedValue" -> Some(x.peak.normalizedValue)
-          ))
-
         new MSSpectra {
           //        logger.info(s"creating MS peak")
           override val uniqueMass: Option[Double] = None
@@ -83,9 +79,11 @@ object MSDialLCMSProcessedSample {
           override val associatedScan: Option[SpectrumProperties] = _properties
           override val metadata: Map[String, AnyRef] = _metadata
         }
-      } else {
+      }
 
+      else {
         val _accurateMass = x.ms1AccurateMass
+
         val _precursorScan = Some(new SpectrumProperties {
           override val msLevel: Short = 1
           override val modelIons: Option[List[Double]] = None
@@ -110,10 +108,7 @@ object MSDialLCMSProcessedSample {
           override val massOfDetectedFeature: Option[Ion] = _massOfDetectedFeature
           override val precursorScan: Option[SpectrumProperties] = _precursorScan
           override val associatedScan: Option[SpectrumProperties] = _associatedScan
-          /**
-            * Contains random metadata associated to the object we mix this into
-            */
-          override val metadata: Map[String, AnyRef] = Map()
+          override val metadata: Map[String, AnyRef] = _metadata
         }
       }
     }
